6706e4b163f357061b61ee4078e473f4e2d76501
kblee <kblee0@gmail.com>
2025-03-09 15:28:31 +0900

    add awake funciton

--- cm/__main__.py
+++ cm/__main__.py
@@ -9,12 +9,14 @@ from cm.TrayIcon import TrayIcon
 def main():
     me = singleton.SingleInstance()
     log_file = tempfile.gettempdir() + '\\cm.log'
-    handler = TimedRotatingFileHandler(log_file,
+    file_handler = TimedRotatingFileHandler(log_file,
                                        when="d",
                                        interval=1,
                                        backupCount=5)
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
 
-    logging.basicConfig(handlers=[handler], format='%(asctime)s [%(levelname)s]: %(message)s', level=logging.INFO)
+    logging.basicConfig(handlers=[file_handler,console_handler], format='%(asctime)s [%(levelname)s]: %(message)s', level=logging.INFO)
     logging.info("----------------------------------------------")
     logging.info("ClipboardManager starting")
     tray_icon = TrayIcon()
