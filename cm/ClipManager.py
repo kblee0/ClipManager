90bc4a6b4e0af0c10721ce2aade0da1ace53cfc0
kblee <kblee0@gmail.com>
2025-03-11 01:14:04 +0900

    text format bug fix

--- cm/ClipManager.py
+++ cm/ClipManager.py
@@ -12,13 +12,14 @@ import win32gui
 # Store the supported formats
 SUPPORTED_CF = (
     win32clipboard.RegisterClipboardFormat("Rich Text Format"),
-    # win32clipboard.CF_TEXT,  # 1: Text
+    win32clipboard.CF_TEXT,  # 1: Text
     # win32clipboard.CF_OEMTEXT,
     win32clipboard.CF_LOCALE,
     # win32clipboard.CF_BITMAP,
     # win32clipboard.CF_DIB,
     win32clipboard.CF_DIBV5,  # 17: Images
-    # win32clipboard.CF_ENHMETAFILE
+    # win32clipboard.CF_ENHMETAFILE,
+    # win32clipboard.CF_METAFILEPICT,
     win32clipboard.CF_UNICODETEXT,  # 13: Unicode Text
     win32clipboard.RegisterClipboardFormat("HTML Format"),
     win32clipboard.RegisterClipboardFormat("image/svg+xml"),
@@ -148,14 +149,10 @@ class Clipboard:
 
                 data = self._clip_data[format]
 
-                # Error avoidance logic for specific data
-                # Example: 12345\r\n
-                if format == win32clipboard.CF_UNICODETEXT:
-                    if isinstance(data, bytes): data = data.decode("utf-8")
-                    if data.find("\r") >= 0 and format != win32clipboard.CF_UNICODETEXT: continue
-                    data = data.replace("\r\n", "\n").replace("\r", "\n").encode("utf-16-le")
-
-                win32clipboard.SetClipboardData(format, data)
+                if format == win32clipboard.CF_UNICODETEXT or format == win32clipboard.CF_TEXT:
+                    win32clipboard.SetClipboardText(data, format)
+                else:
+                    win32clipboard.SetClipboardData(format, data)
                 logging.debug("* Restore :: format = %s(%d), size = %d", format_name, format,
                               len(self._clip_data[format]))
             except:
